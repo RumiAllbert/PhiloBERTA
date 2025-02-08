e4569c5fbbf812868956169ac828a705c04751ae
Rumi Allbert <rumiallbert@gmail.com>
2025-02-08 21:00:50 +0100

    Add test sentences for Greek and Latin languages

---
+++ philoberta/test_data.py
@@ -0,0 +1,16 @@
+TEST_SENTENCES = {
+    "greek": [
+        "ὁ λόγος ἐν ἀρχῇ ἦν",
+        "ἡ ψυχὴ ἀθάνατός ἐστιν",
+        "ἡ ἀρετὴ γνῶσίς ἐστιν",
+        "ἡ σοφία τῆς ψυχῆς κόσμος ἐστίν",
+        "ὁ νοῦς πάντων ἡγεμών",
+    ],
+    "latin": [
+        "ratio et oratio",
+        "anima immortalis est",
+        "virtus scientia est",
+        "sapientia ornamentum animi est",
+        "intellectus omnium dux",
+    ],
+}
